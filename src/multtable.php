18a43272d4761dbc5f75f112ceab87b5e0d4d5d6
fitzsimk <kfitzsimmons11@gmail.com>
2015-04-30 11:40:53 -0700

    added error handling worked for one hour 10:40-11:40am

--- src/multtable.php
+++ src/multtable.php
@@ -4,6 +4,7 @@
   - max-multiplicand
   - min-multiplier
   - max-multiplier
+http://chrome-warrior-75-222926.usw1-2.nitrousbox.com/multtable.php?min-multiplicand=6&max-multiplicand=20&min-multiplier=1
 
 It should check than the min is in fact less than or equal to the max
  multiplicand and multiplier respectively. If it is not, it should print the
@@ -27,6 +28,71 @@ To accomplish the above task you will want to work with loops to dynamically
  create rows and within each row, loop to create the cells. It should output
  as a valid HTML5 document. -->
 <?php
-$response = http_get("http://web.engr.oregonstate.edu/~fitzsimk/PHP-Assignment-1/src/multtable.php", $min_multiplicand, $max_multiplicand,$min_multiplier,$max_multiplier);
-print_r($min_multiplicand);
+
+//check for empty parameter
+if(empty( $_GET['min-multiplicand'])||empty($_GET['max-multiplicand'])||empty($_GET['min-multiplier'])|| empty($_GET['max-multiplier']) ){
+  echo "Missing parameter ";
+
+  if(empty($_GET['min-multiplicand'])){
+    echo "min-multiplicand ";
+  }
+
+  if(empty($_GET['max-multiplicand'])){
+    echo "max-multiplicand ";
+  }
+
+  if(empty($_GET['min-multiplier'])){
+    echo "min-multiplier ";
+  }
+
+  if(empty($_GET['max-multiplier'])){
+    echo "max-multiplier ";
+  }
+}
+
+
+//assign to variables
+$min_cand = isset($_GET['min-multiplicand']) ? $_GET['min-multiplicand'] : ' ';
+$max_cand = isset($_GET['max-multiplicand']) ? $_GET['max-multiplicand'] : ' ';
+$min_plier = isset($_GET['min-multiplier']) ? $_GET['min-multiplier'] : ' ';
+$max_plier = isset($_GET['max-multiplier']) ? $_GET['max-multiplier'] : ' ';
+
+//Check for integer
+if(ctype_digit($min_cand) != true){
+//  $type = gettype($min_cand);
+  echo "Min-multiplicand must be an integer! ";
+}else{
+  $min_cand = (int)$min_cand;
+}
+
+if(ctype_digit($max_cand) != true){
+    echo "Max-multiplicand must be an integer!";
+}else{
+  $max_cand = (int)$max_cand;
+}
+
+if(ctype_digit($min_plier) != true){
+    echo "Min-multiplier must be an integer!";
+}else{
+   $min_plier = (int)$min_plier;
+}
+
+if(ctype_digit($max_plier) != true){
+    echo "Max-multiplier must be an integer!";
+}else{
+     $max_plier = (int)$max_plier;
+}
+
+
+// It should check than the min is in fact less than or equal to the max
+//  multiplicand and multiplier respectively. If it is not, it should print the
+//   message "Minimum [multiplicand|multiplier] larger than maximum.".
+
+//  It should
+//   also check that the values returned are integers for each parameter. If it is
+//   not it should print 1 message for each invalid input
+//   "[min-multiplicand...max-multiplier]
+//   must be an integer.".
+
+
 ?>

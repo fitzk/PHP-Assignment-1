84a75de0e6ad29c74ac6a15052f43bb5eb781d96
fitzsimk <kfitzsimmons11@gmail.com>
2015-05-01 21:13:53 -0700

    moved redirect login to header

--- src/login/content2.php
+++ src/login/content2.php
@@ -2,6 +2,13 @@
 session_start();
 // error_reporting(E_ALL);
 // ini_set('display_errors','On');
+$filePath = explode('/', $_SERVER['PHP_SELF'],-1);
+$filePath = implode('/',$filePath);
+$redirect= "http://" . $_SERVER['HTTP_HOST'].$filePath;
+
+if($_SESSION['correct'] != true)){
+  header("Location: {$redirect}/Login.php", true);
+  }
 ?>
 <!Doctype html>
 <html>
@@ -10,15 +17,9 @@ session_start();
   </head>
 <body>
   <?PHP
-  $filePath = explode('/', $_SERVER['PHP_SELF'],-1);
-  $filePath = implode('/',$filePath);
-  $redirect= "http://" . $_SERVER['HTTP_HOST'].$filePath;
 
-  if($_SESSION['correct'] != true){
-    header("Location: {$redirect}/Login.php", true);
-  }else{
-    echo '<a href="content1.php">content1</a>';
-  }
+
+  echo '<a href="content1.php">content1</a>';
 
   ?>
 </body>

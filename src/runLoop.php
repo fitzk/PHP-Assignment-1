de9d57f83b21d02a53382952c075ab4c03a69fb9
fitzsimk <kfitzsimmons11@gmail.com>
2015-05-01 19:35:04 -0700

    adding correct run file

---
+++ src/runLoop.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+session_start();
+?>
+<!Doctype html>
+<html>
+  <head>
+    <title>Login Page</title>
+  </head>
+<body>
+<form id="loginForm" method = "POST" action= "loopback.php">
+ <input type="text" name= "a">
+<input type="text" name= "b">
+<input type="text" name= "c">
+<input type="text" name= "d">
+<input type="text" name= "e">
+<input type="text" name= "f">
+<input type="text" name= "g">
+<input type="text" name= "h">
+<input type="submit">
+</form>
+
+</body>
+  </html>
